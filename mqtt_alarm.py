a65c37c663c4349fcfab2e084656cb0994229d4d
Steven Coutts <stevec@couttsnet.com>
2020-07-27 09:23:47 +0100

    Try to fix subscribe function

--- mqtt_alarm.py
+++ mqtt_alarm.py
@@ -37,12 +37,11 @@ PIN_MAP = {
 GPIO.setmode(GPIO.BOARD)
 
 
-def on_connect(client, userdata, flags, rc):  # The callback for when the client connects to the broker
-  print("Connected with result code {0}".format(str(rc)))  # Print result of connection attempt
-  client.subscribe("digitest/test1")  # Subscribe to the topic “digitest/test1”, receive any messages published on it
+def on_connect(client, userdata, flags, rc):
+  print("Connected with result code {0}".format(str(rc)))
+  client.subscribe("garagedoor")
 
-
-def on_message(client, userdata, msg):  # The callback for when a PUBLISH message is received from the server.
+def on_message(client, userdata, msg):
   print("Message received-> " + msg.topic + " " + str(msg.payload))  # Print a received msg
 
 
