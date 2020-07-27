b7080a5aadbc35f7b7923762e99f532c54f70f22
Steven Coutts <stevec@couttsnet.com>
2020-07-27 16:56:44 +0100

    Change to BCM

--- mqtt_subscriber.py
+++ mqtt_subscriber.py
@@ -17,34 +17,37 @@ MQTT_TOPIC_PREFIX = "garagedoor/door-up"
 MQTT_TOPIC_PREFIX2 = "garagedoor/door-down"
 MQTT_CLIENT_ID = "garagepi"
 
-GPIO.setmode(GPIO.BCM)
-
 def on_connect(client, userdata, flags, rc):
   print("Connected with result code {0}".format(str(rc)))
   client.subscribe(MQTT_TOPIC_PREFIX)
   client.subscribe(MQTT_TOPIC_PREFIX2)
 
 def on_message(client, userdata, msg):
+  GPIO.setmode(GPIO.BCM)
   print("Message received-> " + msg.topic + " " + str(msg.payload))
   if 'door-up' in msg.topic:
     if 'off' in msg.payload:
       print 'Door up Turn Off - disable GPIO Here'
       GPIO.setup(4, GPIO.OUT)
       GPIO.output(4, 0)
+      GPIO.cleanup()
     elif 'on' in msg.payload:
       print 'Door up Turn On - enable GPIO Here'
       GPIO.setup(4, GPIO.OUT)
       GPIO.output(4, 1)
+      GPIO.cleanup()
   elif 'door-down' in msg.topic:
     if 'off' in msg.payload:
       print 'Door down Turn Off - disable GPIO Here'
       GPIO.setup(priv.pindown, GPIO.OUT)
       GPIO.output(priv.pindown, 0)
+      GPIO.cleanup()
     elif 'on' in msg.payload:
       print 'Door down Turn On - enable GPIO Here'
       GPIO.setup(priv.pindown, GPIO.OUT)
       GPIO.output(priv.pindown, 1)
-  GPIO.cleanup()
+      GPIO.cleanup()
+
 
 def subscribe_topic():
   client = mqtt.Client()
