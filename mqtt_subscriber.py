43c7f17c0246937818cdedd3b8fcc446a21493aa
Steven Coutts <stevec@couttsnet.com>
2020-07-27 15:22:44 +0100

    Try to actually set the GPIO

--- mqtt_subscriber.py
+++ mqtt_subscriber.py
@@ -29,6 +29,11 @@ def on_message(client, userdata, msg):
   if 'door-up' in msg.topic:
     if 'off' in msg.payload:
       print 'Door up Turn Off - enable GPIO Here'
+      GPIO.setup(4, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+      GPIO.add_event_detect(pin, GPIO.BOTH, callback=state_change_hadler, bouncetime=100)
+      state = GPIO.input(pin)
+      log.info("Mapped pin {:0>2d} to {}".format(pin, name))
+      log.info("... state {}".format(state))
     elif 'on' in msg.payload:
       print 'Door up Turn On - enable GPIO Here'
 
