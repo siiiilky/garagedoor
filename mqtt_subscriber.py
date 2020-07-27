612d19a821c8c6816a72112962a2d013fed5de88
Steven Coutts <stevec@couttsnet.com>
2020-07-27 17:25:20 +0100

    Move more variables

--- mqtt_subscriber.py
+++ mqtt_subscriber.py
@@ -12,19 +12,14 @@ log = logging.getLogger('mqtt_alarm')
 log.addHandler(JournalHandler())
 log.setLevel(logging.INFO)
 
-MQTT_HOST = "10.100.30.6"
-MQTT_TOPIC_PREFIX = "garagedoor/door-up"
-MQTT_TOPIC_PREFIX2 = "garagedoor/door-down"
-MQTT_CLIENT_ID = "garagepi"
-
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(17, GPIO.OUT)
-GPIO.setup(4, GPIO.OUT)
+GPIO.setup(priv.pindown, GPIO.OUT)
+GPIO.setup(priv.pinup, GPIO.OUT)
 
 def on_connect(client, userdata, flags, rc):
   print("Connected with result code {0}".format(str(rc)))
-  client.subscribe(MQTT_TOPIC_PREFIX)
-  client.subscribe(MQTT_TOPIC_PREFIX2)
+  client.subscribe(priv.MQTT_TOPIC_PREFIX)
+  client.subscribe(priv.MQTT_TOPIC_PREFIX2)
 
 def on_message(client, userdata, msg):
   print("Message received-> " + msg.topic + " " + str(msg.payload))
@@ -48,7 +43,7 @@ def subscribe_topic():
   client.on_connect = on_connect
   client.on_message = on_message
   client.username_pw_set(username=priv.username, password=priv.password)
-  client.connect(MQTT_HOST, 1883)
+  client.connect(priv.MQTT_HOST, 1883)
   client.loop_forever()  # Start networking daemon
 
 subscribe_topic()
