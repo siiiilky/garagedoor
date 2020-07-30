87653b7bab5bb8e4864357820e854d4daa5645cc
Steven Coutts <stevec@couttsnet.com>
2020-07-30 07:24:14 +0100

    Change sleep time

--- lcd-display.py
+++ lcd-display.py
@@ -142,7 +142,7 @@ def main():
       lcd_string("Garage CLOSED", LCD_LINE_2)
     else:
       lcd_string("Garage OPEN", LCD_LINE_2)
-    time.sleep(30)
+    time.sleep(240)
 
 if __name__ == '__main__':
 
